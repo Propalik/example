b25e4362185e2eab819817250256afdfba3a389c
Андрей Петров <andrey_petrov_2017@mail.ru>
2023-10-24 11:09:09 +0300

    skeleton

--- src/js/tasks.js
+++ src/js/tasks.js
@@ -1,8 +1,16 @@
+// Skeleton
+window.addEventListener("load", () => {
+    const skeleton = document.querySelectorAll(".skeleton");
+    skeleton.forEach(function (element) {
+        element.classList.remove("skeleton");
+    });
+});
+
 // Иконки heart /
-const hearts = document.querySelectorAll('.card-heart');
+const hearts = document.querySelectorAll(".card-heart");
 
 hearts.forEach(heart => {
-    heart.addEventListener('click', () => {
-        heart.classList.toggle('heart-active');
+    heart.addEventListener("click", () => {
+        heart.classList.toggle("heart-active");
     });
 });
